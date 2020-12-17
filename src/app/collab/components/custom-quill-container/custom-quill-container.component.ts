507eb56812b9f7f681a773b8101af3f90fa189f0
Dominik Walser <jehob@web.de>
2020-12-17 09:58:54 +0100

    editor leaner

--- src/app/collab/components/custom-quill-container/custom-quill-container.component.ts
+++ src/app/collab/components/custom-quill-container/custom-quill-container.component.ts
@@ -100,18 +100,16 @@ export class CustomQuillContainerComponent implements OnInit, OnChanges, OnDestr
 
             [{ list: 'ordered' }, { list: 'bullet' }],
             [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-            [{ direction: 'rtl' }], // text direction
 
             // [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
             [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
             [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-            [{ font: [] }],
             [{ align: [] }],
 
             ['clean'], // remove formatting button
 
-            ['link', 'image', 'video'] // link and image, video
+            ['link'] // link and image, video
         ],
         cursors: true
     };
