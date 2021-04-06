74f0c7d4689796f1f2049b62f75e43af2e284b29
danielmoessner <github@moesibert.de>
2021-04-06 22:54:17 +0200

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: [
+   'plugin:@angular-eslint/recommended',
+   'plugin:@typescript-eslint/recommended',
+   'plugin:@typescript-eslint/recommended-requiring-type-checking',
+   'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  }
+ };
