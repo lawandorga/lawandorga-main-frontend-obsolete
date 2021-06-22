6320802990b480e8826068c5690327658788d803
danielmoessner <github@moesibert.de>
2021-06-22 17:07:45 +0200

    updates

--- src/app/statics/api_urls.statics.ts
+++ src/app/statics/api_urls.statics.ts
@@ -1,24 +1,3 @@
-/*
- * law&orga - record and organization management software for refugee law clinics
- * Copyright (C) 2019  Dominik Walser
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>
- */
-
-import { SearchParamsInterface } from '../shared/interfaces/search_params.interface';
-
-// TODO: check
 export const base = ''; // environment.apiUrl;
 export const LOGIN_API_URL = base + 'api/profiles/login/';
 export const RECORDS_API_URL = base + 'api/records/records/';
@@ -64,9 +43,6 @@ const COLLAB_TEXT_DOCUMENT_VERSIONS = base + 'api/collab/text_document_version/'
 const CHECK_USER_ACTIVATION_API_URL = base + 'api/profiles/';
 const ACTIVATE_USER_ACTIVATION_API_URL = base + 'api/activate_user_activation_link/';
 const RESET_PASSWORD_API_URL = base + 'api/profiles/';
-const SPECIAL_RECORD_BASE_API_URL = base + 'api/records/records/'; // deprecated??? is it?
-const DOWNLOAD_SIGNING_BASE_API_URL = base + 'api/storage_down/'; // deprecated
-const RECORD_DOCUMENT_BASE_API_URL = base + 'api/records/documents/';
 const PERMISSION_FOR_GROUP_BASE_API_URL = base + 'api/permissions_for_group/';
 
 export const GetProfilesDetailApiUrl = (id: number) => {
