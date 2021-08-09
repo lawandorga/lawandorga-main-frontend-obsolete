ddb6150e03ca8c03f29ec198c873e06686648c29
danielmoessner <github@moesibert.de>
2021-08-09 17:04:52 +0200

    bug fix

--- src/app/shared/other/download.ts
+++ src/app/shared/other/download.ts
@@ -2,15 +2,12 @@ import { HttpResponse } from '@angular/common/http';
 
 export default function downloadFile(response: HttpResponse<Blob>, name: string): void {
   const filename: string = name;
-  const binaryData = [];
-  binaryData.push(response.body);
-  const file = new Blob(binaryData);
   if (name.split('.').pop() === 'pdf') {
-    const fileUrl = URL.createObjectURL(file);
+    const fileUrl = window.URL.createObjectURL(response.body);
     window.open(fileUrl);
   } else {
     const downloadLink = document.createElement('a');
-    downloadLink.href = window.URL.createObjectURL(file);
+    downloadLink.href = window.URL.createObjectURL(response.body);
     downloadLink.setAttribute('download', filename);
     document.body.appendChild(downloadLink);
     downloadLink.click();
