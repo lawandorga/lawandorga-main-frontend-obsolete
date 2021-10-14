7526a7a08224ed865782989af4b2f0af539627f4
danielmoessner <github@moesibert.de>
2021-10-14 17:48:00 +0200

    create permission list and improve ui

--- src/app/core/components/admin/admin.component.ts
+++ src/app/core/components/admin/admin.component.ts
@@ -14,6 +14,7 @@ export class AdminComponent implements OnInit {
       link: '/profiles',
       icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>',
       notifications: 0,
+      class: 'rounded-tl-lg',
     },
     {
       title: 'Groups',
@@ -22,6 +23,7 @@ export class AdminComponent implements OnInit {
       link: '/groups',
       icon: '<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" /></svg>',
       notifications: 0,
+      class: 'rounded-tr-lg',
     },
     {
       title: 'Tags',
@@ -29,6 +31,7 @@ export class AdminComponent implements OnInit {
       link: '/records/tags',
       icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" /></svg>',
       notifications: 0,
+      class: '',
     },
     {
       title: 'Record-Permit-Requests',
@@ -37,6 +40,7 @@ export class AdminComponent implements OnInit {
       link: '/records/permit-requests',
       icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" /></svg>',
       notifications: 0,
+      class: '',
     },
     {
       title: 'Record-Deletion-Requests',
@@ -44,6 +48,7 @@ export class AdminComponent implements OnInit {
       link: '/records/deletion-requests',
       icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>',
       notifications: 0,
+      class: 'rounded-bl-lg',
     },
   ];
 
--- src/app/core/components/permission-list/permission-list.component.ts
+++ src/app/core/components/permission-list/permission-list.component.ts
@@ -1,17 +1,105 @@
-import { Component, OnInit } from '@angular/core';
-import { Permission } from '../../models/permission.model';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-permission-list',
   templateUrl: './permission-list.component.html',
 })
-export class PermissionListComponent implements OnInit {
-  permissions: Permission[];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.http.get('api/permissions/').subscribe((response: Permission[]) => (this.permissions = response));
-  }
+export class PermissionListComponent {
+  permissions = [
+    {
+      name: 'accept_new_users_rlc',
+      explanation: 'Zulassung neuer Nutzer*innen',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'access_to_files_rlc',
+      explanation: 'Genereller Zugriff aufs Dateimanagement (Files)',
+      group: 'Alle Nutzer*innen',
+    },
+    {
+      name: 'add_record_rlc',
+      explanation: 'Akten anlegen',
+      group: 'Berater*innen',
+    },
+    {
+      name: 'can_consult',
+      explanation: 'Nutzer*in kann als „Consultant“ (Berater*in) eingetragen werden',
+      group: 'Berater*innen',
+    },
+    {
+      name: 'manage_collab_document_permissons_rlc',
+      explanation: 'Verwaltung der Berechtigungen für Collab',
+      group: 'Administrator*innen/ Vorstand',
+    },
+    {
+      name: 'manage_folder_permissions_rlc',
+      explanation: 'Verwaltung der Berechtigungen im Dateimanagement',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'manage_group',
+      explanation: 'Verwaltung einzelner Gruppe',
+      group: 'Entsprechendes Ressort',
+    },
+    {
+      name: 'manage_groups_rlc',
+      explanation: 'Verwaltung aller Gruppen',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'manage_permissions_rlc',
+      explanation: 'Allgemeine Berechtigungsverwaltung',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'manage_users',
+      explanation: 'Verwaltung der Nutzer*innen',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'permit_record_permission_requests_rlc',
+      explanation: 'Bearbeitung von Aktenzugriffsanfragen',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'process_record_deletion_requests',
+      explanation: 'Bearbeitung von Aktenlöschanfragen',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'read_all_collab_documents_rlc',
+      explanation: 'Zugriff auf alle Collab Dokumente',
+      group: 'Aller Nutzer*innen',
+    },
+    {
+      name: 'read_all_folders_rlc',
+      explanation: 'Genereller Lesezugriff auf alle Ordner im Dateimanagement',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'view_permissions_rlc',
+      explanation: 'Sichtbarkeit von allgemeinen Berechtigungen',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'view_records_full_detail_rlc',
+      explanation: 'Vollständiger Zugriff auf alle Akten',
+      group: 'Administrator*innen / Vorstand',
+    },
+    {
+      name: 'view_records_rlc',
+      explanation: 'Zugriff auf Grundinformationen der Akten; Sichtbarkeit von Reiter „Records“',
+      group: 'Administrator*innen / Vorstand / Berater*innen',
+    },
+    {
+      name: 'write_all_collab_documents_rlc',
+      explanation: 'Genereller Schreibzugriff auf Collab Dokumente',
+      group: 'Alle Nutzer*innen',
+    },
+    {
+      name: 'write_all_folders_rlc',
+      explanation: 'Genereller Schreibzugriff auf Ordner im Dateimanagement',
+      group: 'Administrator*innen / Vorstand',
+    },
+  ];
 }
